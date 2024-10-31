3660c8aabe6e256443c7638f1a5aef077378e66a
st9 <eowjd112500@gmail.com>
2024-11-01 03:24:29 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -61,6 +61,34 @@ KillingCheats:CreateButton("날로먹는 스테이지 타워", function()
     teleportToPoints()
 end)
 
+KillingCheats:CreateButton("붕어빵 스테이지 타워", function()
+    local player = game.Players.LocalPlayer
+    local character = player.Character or player.CharacterAdded:Wait()
+    local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+    local pointFolder = workspace:WaitForChild("PointFolder")
+
+    local points = {}
+
+    for i = 1, 1471 do  
+        local point = pointFolder:FindFirstChild(tostring(i))
+        if point and point:IsA("Part") then
+            table.insert(points, point)
+        end
+    end
+
+    isTeleporting = true  -- Start teleporting
+
+    local function teleportToPoints()
+        for _, point in ipairs(points) do
+            if not isTeleporting then break end  -- Check if teleporting should stop
+            humanoidRootPart.CFrame = point.CFrame
+            wait(0)  
+        end
+    end
+
+    teleportToPoints()
+end)
+
 KillingCheats:CreateButton("stop", function()
     print("Teleportation stopped")
     isTeleporting = false  -- Stop teleporting
